1b03f7bbe404ac8dc2b532d873b3799e8bd838fa
Alastair Lockie <alastair@brandwatch.com>
2022-03-30 17:22:10 +0100

    Add util for quickly copying dotfiles

---
+++ scripts/copy_dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~
+ln -s dotfiles/.gitconfig .gitconfig 
+ln -s dotfiles/.gitignore .gitignore 
+ln -s dotfiles/.bash_aliases .bash_aliases
+ln -s dotfiles/.bash_profile .bash_profile
