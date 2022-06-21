8e396b1ba0d181e75c301b2081912c9602265a4b
Alastair Lockie <alastair@brandwatch.com>
2022-06-21 16:55:04 +0100

    Add zshell and associated plugins etc

--- scripts/copy_dotfiles.sh
+++ scripts/copy_dotfiles.sh
@@ -5,3 +5,6 @@ ln -s dotfiles/.gitconfig .gitconfig
 ln -s dotfiles/.gitignore .gitignore 
 ln -s dotfiles/.bash_aliases .bash_aliases
 ln -s dotfiles/.bash_profile .bash_profile
+ln -s dotfiles/.zshrc .zshrc
+ln -s dotfiles/.p10k.zsh .p10k.zsh
+ln -s dotfiles/.zprofile .zprofile
