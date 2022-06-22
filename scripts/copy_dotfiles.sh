b8134ee18f0a3be860f69e3a0c27da7ab1cae28a
Alastair Lockie <alastair@brandwatch.com>
2022-06-22 16:43:30 +0100

    Also copy vimrc

--- scripts/copy_dotfiles.sh
+++ scripts/copy_dotfiles.sh
@@ -5,6 +5,7 @@ ln -s dotfiles/.gitconfig .gitconfig
 ln -s dotfiles/.gitignore .gitignore 
 ln -s dotfiles/.bash_aliases .bash_aliases
 ln -s dotfiles/.bash_profile .bash_profile
+ln -s dotfiles/.vimrc .vimrc
 ln -s dotfiles/.zshrc .zshrc
 ln -s dotfiles/.p10k.zsh .p10k.zsh
 ln -s dotfiles/.zprofile .zprofile
