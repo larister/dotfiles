52c7b477752d64864bbcc67d006634f5a3acc808
Alastair Lockie <alastair@brandwatch.com>
2023-02-11 10:27:52 +0000

    add oh-my-zsh

--- scripts/copy_dotfiles.sh
+++ scripts/copy_dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~
+mkdir .oh-my-zsh
 ln -s dotfiles/.gitconfig .gitconfig 
 ln -s dotfiles/.gitignore .gitignore 
 ln -s dotfiles/.bash_aliases .bash_aliases
@@ -9,3 +10,4 @@ ln -s dotfiles/.vimrc .vimrc
 ln -s dotfiles/.zshrc .zshrc
 ln -s dotfiles/.p10k.zsh .p10k.zsh
 ln -s dotfiles/.zprofile .zprofile
+ln -s dotfiles/oh-my-zsh.sh .oh-my-zsh/oh-my-zsh.sh
